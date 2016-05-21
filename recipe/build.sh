119c3891040edd40ead518aab2922b1d597182fe
Amir Mohammadi <183.amir@gmail.com>
2016-05-21 10:32:06 +0200

    Add -headerpad_max_install_names

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,7 @@
 
 if [[ `uname` == 'Darwin' ]]; then
     export MACOSX_DEPLOYMENT_TARGET=10.9
+    export CFLAGS="${CFLAGS} -headerpad_max_install_names"
 fi
 
 $PYTHON -B setup.py install --single-version-externally-managed --record record.txt
